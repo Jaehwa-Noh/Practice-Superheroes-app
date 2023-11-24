2dd43598478af192b45daff7ff08002691669fac
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-24 16:43:33 +0900

    Create HeroesScreen and add HeroesList, HeroesListItem composable.

---
+++ Compose-Superheros/app/src/main/java/com/example/superheros/HeroesScreen.kt
@@ -0,0 +1,14 @@
+package com.example.superheros
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HeroesList(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun HeroesListItem(modifier: Modifier = Modifier) {
+
+}
