0fb891feef116c7448bb453dbe99e2360a55d78c
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-24 21:38:03 +0900

    Create Hero List and apply padding.

--- Compose-Superheros/app/src/main/java/com/example/superheros/HeroesScreen.kt
+++ Compose-Superheros/app/src/main/java/com/example/superheros/HeroesScreen.kt
@@ -3,10 +3,13 @@ package com.example.superheros
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,12 +21,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.superheros.data.HeroesRepository
 import com.example.superheros.model.Hero
 import com.example.superheros.ui.theme.SuperherosTheme
 
 @Composable
 fun HeroesList(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(HeroesRepository.heroes) { hero ->
+            HeroesListItem(
+                hero = hero,
+                modifier = Modifier.padding(vertical = 4.dp)
+            )
+        }
 
+    }
 }
 
 @Composable
