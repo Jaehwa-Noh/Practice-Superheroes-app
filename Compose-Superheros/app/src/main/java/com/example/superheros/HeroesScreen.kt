64c8216f554e470879365aa50f41a4afab12c0ed
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-24 16:46:11 +0900

    Create preview.

--- Compose-Superheros/app/src/main/java/com/example/superheros/HeroesScreen.kt
+++ Compose-Superheros/app/src/main/java/com/example/superheros/HeroesScreen.kt
@@ -2,6 +2,7 @@ package com.example.superheros
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HeroesList(modifier: Modifier = Modifier) {
@@ -11,4 +12,16 @@ fun HeroesList(modifier: Modifier = Modifier) {
 @Composable
 fun HeroesListItem(modifier: Modifier = Modifier) {
 
+}
+
+@Preview
+@Composable
+fun HeroesListPreview() {
+    HeroesList()
+}
+
+@Preview
+@Composable
+fun HeroesListItemPreview() {
+    HeroesListItem()
 }
