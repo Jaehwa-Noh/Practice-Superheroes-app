bb80745a93ae2080666794370f5271c3e37a7433
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-26 11:15:30 +0900

    Create Card View.

---
+++ SwiftUI-Superheores/Super Heores/HeroesScreen.swift
@@ -0,0 +1,60 @@
+//
+//  HeroesScreen.swift
+//  Super Heores
+//
+//  Created by Jaehwa Noh on 11/26/23.
+//
+
+import SwiftUI
+
+struct HeroesScreen: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct HeroesList: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct HeroesListItem: View {
+    let hero: Hero
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Text(hero.name)
+                    Spacer(minLength: 0)
+                }
+                HStack(spacing: 0) {
+                    Text(hero.description)
+                    Spacer(minLength: 0)
+                }
+            }
+            
+            
+            Spacer()
+                .frame(width: 16)
+            Image(hero.imageResourceName)
+                .resizable()
+                .frame(width:72, height:72)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+        .padding(16)
+        .background(
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(Color("CardColor"))
+        )
+    }
+}
+
+#Preview {
+    HeroesListItem(hero: HeroesRepository().heroes.first!)
+}
+
+#Preview {
+    HeroesListItem(hero: HeroesRepository().heroes.first!)
+}
