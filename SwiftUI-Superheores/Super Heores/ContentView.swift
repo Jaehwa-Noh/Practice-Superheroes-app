3397dfc9e61752dcb8b8203bfd2fb0f64497a67e
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-26 11:22:15 +0900

    Apply HeroesList.

--- SwiftUI-Superheores/Super Heores/ContentView.swift
+++ SwiftUI-Superheores/Super Heores/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                
-            }
+            HeroesList()
             .navigationTitle("Superheroes")
             .navigationBarTitleDisplayMode(.large)
         }
