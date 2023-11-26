5b306d1edc8c8be8274180b8c6bd14e361c79893
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-26 10:23:32 +0900

    Add Navigation Title.

--- SwiftUI-Superheores/Super Heores/ContentView.swift
+++ SwiftUI-Superheores/Super Heores/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                
+            }
+            .navigationTitle("Superheroes")
+            .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
+        
+        
     }
 }
 
