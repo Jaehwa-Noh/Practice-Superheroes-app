0f82455d4303d0f5106ae649fe8da3a8ab4b0e4c
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-26 11:24:43 +0900

    Change NavigationView to NavigationStack.

--- SwiftUI-Superheores/Super Heores/ContentView.swift
+++ SwiftUI-Superheores/Super Heores/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
+        NavigationStack {
             HeroesList()
             .navigationTitle("Superheroes")
             .navigationBarTitleDisplayMode(.large)
         }
-        
-        
     }
 }
 
